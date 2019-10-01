4fc44901531eeb3c9995ede77b7f750f4bc40e31
wrdac <wrydacuyan@gmail.com>
2019-10-01 21:00:18 +0800

    smoother transition of footer on scroll

--- website/js/footer.js
+++ website/js/footer.js
@@ -1,16 +1,16 @@
 $(window).scroll(function() {
-    if($(window).scrollTop() + $(window).height() > $(document).height() - 10) {
+    if($(window).scrollTop() + $(window).height() > $(document).height() - 40) {
+        $("footer").removeClass("bgcolor-revaniamate");
         $("footer").addClass("bgcolor-animate");
         $(".foot-style").css({
-            "background": "linear-gradient(#ffff96, #50cf30)",
-            "transition": "background-ease-in 500ms"
+            "background": "linear-gradient(#ffff96, #50cf30)"
         });
     }
     if($(window).scrollTop() + $(window).height() <= $(document).height() - 50){
         $("footer").removeClass("bgcolor-animate");
+        $("footer").addClass("bgcolor-revaniamate");
         $(".foot-style").css({
-            "background": "linear-gradient(transparent, #ffff96)",
-            "transition": "background-ease-in 500ms"
+            "background": "linear-gradient(transparent, #ffff96)"
         });
     }
 });
