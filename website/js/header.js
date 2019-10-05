67f7aab990e6bd67e1b816677a8855ebcfa9a769
wrdac <wrydacuyan@gmail.com>
2019-10-06 02:35:44 +0800

    added new nav on html

--- website/js/header.js
+++ website/js/header.js
@@ -11,16 +11,36 @@ function search(){
     });
     $(".header-search>input").focus();
 }
-$(".header-search>input").focusout(function(){
-    if(screen.width<=540){
-        $(".header-search>input").css({
-            "display": "none"
-        });
-        $(".header-search>button").css({
-            "display": "block"
-        });
-        $(".navbar-brand").css({
-            "display": "block"
-        });
+// $(".header-search>input").focusout(function(){
+//     if(screen.width<=540){
+//         $(".header-search>input").css({
+//             "display": "none"
+//         });
+//         $(".header-search>button").css({
+//             "display": "block"
+//         });
+//         $(".navbar-brand").css({
+//             "display": "block"
+//         });
+//     }
+// });
+$(document).mouseup(function(e) 
+{
+    var container = $(".header-search>input");
+
+    // if the target of the click isn't the container nor a descendant of the container
+    if (!container.is(e.target) && container.has(e.target).length === 0) 
+    {
+        if(screen.width<=540){
+            $(".header-search>input").css({
+                "display": "none"
+            });
+            $(".header-search>button").css({
+                "display": "block"
+            });
+            $(".navbar-brand").css({
+                "display": "block"
+            });
+        }
     }
 });
